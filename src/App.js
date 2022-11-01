78de840e0720a51bdfbd4b679da99fb1a37d269d
sourabh-c <79144707+sourabh-c@users.noreply.github.com>
2022-11-01 15:34:55 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
+
+//JavaScript program to swap two variables
+
+//take input from the users
+let a = prompt('Enter the first variable: ');
+let b = prompt('Enter the second variable: ');
+
+//create a temporary variable
+let temp;
+
+//swap variables
+temp = a;
+a = b;
+b = temp;
+
+console.log(`The value of a after swapping: ${a}`);
+console.log(`The value of b after swapping: ${b}`);
